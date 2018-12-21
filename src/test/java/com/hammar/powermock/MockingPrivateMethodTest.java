eef056248930d6f6f920e7e4f19dd77f35fd1e4d
hammarpojken <nicke_hammar@hotmail.com>
2018-12-21 13:49:15 +0100

    Add test for mocking constructors using powermock

--- src/test/java/com/hammar/powermock/MockingPrivateMethodTest.java
+++ src/test/java/com/hammar/powermock/MockingPrivateMethodTest.java
@@ -1,56 +1,41 @@
 package com.hammar.powermock;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
-
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 
 import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
+
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
-import com.hammar.data.api.TodoService;
-
 @RunWith(PowerMockRunner.class)
 public class MockingPrivateMethodTest {
-	
+
 	@Mock
 	Dependency dependency;
-	
+
 	@InjectMocks
 	SystemUnderTest systemUnderTest;
 
-
 	@Test
 	public void testSystemUnderTest_StaticMethod() throws Exception {
-		
-		List<Integer> stats = Arrays.asList(1,2,3);
-		
+
+		List<Integer> stats = Arrays.asList(1, 2, 3);
+
 		when(dependency.retrieveAllStats()).thenReturn(stats);
 
-		//Using whitebox to mock a private method
+		// Using whitebox to mock a private method
 		long result = Whitebox.invokeMethod(systemUnderTest, "privateMethodUnderTest");
-		
+
 		assertEquals(6, result);
-	
+
 	}
-	
+
 }
